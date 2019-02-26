744d96b2e5cda59f1d4bb5149f7c26251815fb2e
Hellen Ward <hellenwellsward@gmail.com>
2019-02-26 20:02:55 +0000

    Updated converter.

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -1,9 +1,15 @@
-describe("Cooking Ingredients Converter", function() {
 
+
+describe("Cooking Ingredients Converter", function() {
   describe("Temperatures", function() {
     it("Fahrenheit to Celsius", function() {
-      var result = convertFahrenheitToCelsius(100);
-
+      function convertFahrenheitToCelsius(num) {
+        let num2 = (num - 32)/1.8;
+        let num3 = Math.round(num2 * 10) / 10;
+        return num3.toString();
+      };
+      var result = convertFahrenheitToCelsius(100)
+      console.log(result);
       expect(result).toEqual('37.8');
     });
   });
