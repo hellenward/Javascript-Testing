df06e76a0480abe55dfd0dc01bcba554934a1d47
Hellen Ward <hellenwellsward@gmail.com>
2019-02-26 20:28:52 +0000

    Wrote some additional converter functions

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -3,14 +3,31 @@
 describe("Cooking Ingredients Converter", function() {
   describe("Temperatures", function() {
     it("Fahrenheit to Celsius", function() {
-      function convertFahrenheitToCelsius(num) {
-        let num2 = (num - 32)/1.8;
-        let num3 = Math.round(num2 * 10) / 10;
-        return num3.toString();
-      };
       var result = convertFahrenheitToCelsius(100)
       console.log(result);
       expect(result).toEqual('37.8');
     });
+    it("Celsius to Fahrenheit", function () {
+      var result = convertCelsiusToFahrenheit(37.8)
+      expect(result).toEqual('100');
+    });
   });
+  describe("Weight", function() {
+    it("Pounds to kilos", function () {
+      var result = convertPoundsToKilos(50)
+      expect(result).toEqual(22.68);
+    });
+  });
+  describe("Volume", function () {
+    it("Litre To Gallons", function () {
+      var result = convertLitrestoGallons(1)
+      expect(result).toEqual(3.78541);
+    });
+  });
+  describe("distance", function () {
+    it("Miles to Km", function () {
+      var result = convertMilesToKm(1)
+      expect(result).toEqual(1.60934);
+    }); 
+  })
 });
--- src/Converter.js
+++ src/Converter.js
@@ -1,4 +1,29 @@
 /** Unit Converter **/
 function convertFahrenheitToCelsius(temperature) {
-  //conversion code
+    let num2 = (temperature - 32)/1.8;
+    let num3 = Math.round(num2 * 10) / 10;
+    return num3.toString();
+  };
+
+function convertCelsiusToFahrenheit(temperature) {
+  let num2 = (temperature * 1.8) + 32;
+  console.log(num2);
+  let num3 = Math.round(num2 * 10) / 10;
+  console.log(num3);
+  return num3.toString();
+}
+
+function convertPoundsToKilos(pounds) {
+  let kilo = (pounds * 0.4536);
+  return kilo;
+}
+
+function convertLitrestoGallons(litres) {
+  let gallons = (litres * 3.78541);
+  return gallons;
+}
+
+function convertMilesToKm(miles) {
+  let km = (miles * 1.60934);
+  return km; 
 }
